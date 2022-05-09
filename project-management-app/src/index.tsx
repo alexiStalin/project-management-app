6e182aed2f7d072975334bf2b60748d59be9a886
alexiStalin <lesha.stalin@gmail.com>
2022-05-09 12:02:38 +0300

    feat: add store

--- project-management-app/src/index.tsx
+++ project-management-app/src/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import store from './store/store';
+
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
 
